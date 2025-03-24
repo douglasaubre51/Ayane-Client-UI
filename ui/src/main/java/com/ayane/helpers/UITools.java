325bbfa1c3a4219adc40cbd5c5623c0d714623b4
allen <boseallen192@gmail.com>
2025-03-24 21:56:38 +0530

    created basic create account page!

---
+++ ui/src/main/java/com/ayane/helpers/UITools.java
@@ -0,0 +1,18 @@
+package com.ayane.helpers;
+
+import javax.swing.JTextField;
+
+public class UITools {
+    public void clearTextFields(JTextField a, JTextField b) {
+        a.setText("");
+        b.setText("");
+        return;
+    }
+
+    public void clearTextFields(JTextField a, JTextField b, JTextField c) {
+        a.setText("");
+        b.setText("");
+        c.setText("");
+        return;
+    }
+}
